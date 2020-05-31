36530075f4c95c5242e0d21f509ca1d4a8fd6e69
kwonjiun <homecoming1@naver.com>
2020-06-01 00:06:07 +0900

    cheer up!

---
+++ harrypotter.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+	printf("Every great wizard in history has started out\n");
+	printf("As nothing more than we are now, Students\n");
+	printf("If they can do it, Why not us?\n");
+	return 0;
+}
