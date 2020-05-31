25f829bae65dfe595a5cdb19c07a820a51a82142
kwonjiun <homecoming1@naver.com>
2020-06-01 00:25:44 +0900

    I missed first sentence

--- harrypotter.c
+++ harrypotter.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int main()
 {
+	printf("Think of it this way\n");
 	printf("Every great wizard in history has started out\n");
 	printf("As nothing more than we are now, Students\n");
 	printf("If they can do it, Why not us?\n");
